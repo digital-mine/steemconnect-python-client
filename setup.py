34ce559db1458cabcd3f1fa4d03c982651aade5a
DIGITAL MINE <digitalmine23@gmail.com>
2021-03-17 21:24:34 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='steemlogin',
     version='0.0.6',
     packages=['steemlogin'],
-    url='https://github.com/emre/steemconnect',
+    url='https://github.com/digital-mine/steemlogin-python-client',
     license='MIT',
     author='digital-mine',
     author_email='digitalmine23@gmail.com',
